eab476b0ab9ed39a2e4dbd55dcb4b5250deb1a68
haidarrahman30 <haidarrahmans30@gmail.com>
2022-06-09 16:36:23 +0800

    update frontend and rules

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title> @yield('title')  </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  @include('includes.styling')
+
+</head>
+
+<body>
+
+  @include('includes.navbar')
+
+  @include('includes.sidebar')
+
+  <main id="main" class="main">
+
+    @yield('content')
+
+  </main><!-- End #main -->
+
+  @include('includes.footer')
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+</body>
+
+</html>
