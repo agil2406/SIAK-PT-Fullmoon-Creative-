e33d8ed45858a7faed0eed798c02b6681bcaca0b
Lalu Farid <lalufarid18@gmail.com>
2022-06-11 10:55:01 +0800

    migration and rout

---
+++ app/Models/BukuOperasional.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BukuOperasional extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_06_11_021019_create_buku_materials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku_materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('uraian_bm',191);
+            $table->integer('volume_bm');
+            $table->string('satuan_bm',191);
+            $table->string('noBukti_bm',191);
+            $table->integer('jumlah_bm');
+            $table->timestamp('tanggal_bm');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku_materials');
+    }
+};
